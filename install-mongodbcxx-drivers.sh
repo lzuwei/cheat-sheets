55a9e3dce0707934b8a78ac4e2a31a2d9e35c32b
lzuwei <zuwei@iqnect.org>
2015-06-09 17:04:22 +0800

    Update install-mongodbcxx-drivers.sh

--- install-mongodbcxx-drivers.sh
+++ install-mongodbcxx-drivers.sh
@@ -9,3 +9,6 @@ git checkout 26compat
 
 #compile the drivers
 sudo scons --full --use-system-boost --sharedclient install-mongoclient
+
+#for macosx 10.10 and above
+sudo scons --full --use-system-boost --sharedclient --osx-version-min=10.10 install-mongoclient
